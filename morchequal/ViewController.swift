24e181a44b3bcb5f026a00b3a61f75f299a1875a
Erik Jälevik <erikjalevik@fastmail.fm>
2019-05-29 12:22:11 +0200

    Initialise XCode project without storyboards

---
+++ morchequal/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  morchequal
+//
+//  Created by Erik Jälevik on 29.05.19.
+//  Copyright © 2019 Fileside. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
